cfd9ea485f58a9aab32b1cd63bfce6fb16758e03
Sean Quah <seanq@element.io>
2021-11-29 15:53:03 +0000

    Refactor test cases
    * Update docstrings.
    * Rename `pat` to `pattern`.
    * Use `assert(Not)Regex`.

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -18,42 +18,44 @@ from matrix_common.regex import glob_to_regex
 
 class GlobToRegexTestCase(TestCase):
     def test_literal_match(self) -> None:
-        """patterns without wildcards should match"""
-        pat = glob_to_regex("foobaz")
-        self.assertTrue(
-            pat.match("FoobaZ"), "patterns should match and be case-insensitive"
+        """Tests matching against a literal."""
+        pattern = glob_to_regex("foobaz")
+        self.assertRegex(
+            "FoobaZ", pattern, "patterns should match and be case-insensitive"
         )
-        self.assertFalse(
-            pat.match("x foobaz"), "pattern should not match at word boundaries"
+        self.assertNotRegex(
+            "x foobaz", pattern, "pattern should not match at word boundaries"
         )
 
     def test_wildcard_match(self) -> None:
-        pat = glob_to_regex("f?o*baz")
+        """Tests matching with wildcards."""
+        pattern = glob_to_regex("f?o*baz")
 
-        self.assertTrue(
-            pat.match("FoobarbaZ"),
+        self.assertRegex(
+            "FoobarbaZ",
+            pattern,
             "* should match string and pattern should be case-insensitive",
         )
-        self.assertTrue(pat.match("foobaz"), "* should match 0 characters")
-        self.assertFalse(pat.match("fooxaz"), "the character after * must match")
-        self.assertFalse(pat.match("fobbaz"), "? should not match 0 characters")
-        self.assertFalse(pat.match("fiiobaz"), "? should not match 2 characters")
+        self.assertRegex("foobaz", pattern, "* should match 0 characters")
+        self.assertNotRegex("fooxaz", pattern, "the character after * must match")
+        self.assertNotRegex("fobbaz", pattern, "? should not match 0 characters")
+        self.assertNotRegex("fiiobaz", pattern, "? should not match 2 characters")
 
     def test_multi_wildcard(self) -> None:
-        """patterns with multiple wildcards in a row should match"""
-        pat = glob_to_regex("**baz")
-        self.assertTrue(pat.match("agsgsbaz"), "** should match any string")
-        self.assertTrue(pat.match("baz"), "** should match the empty string")
-        self.assertEqual(pat.pattern, r"\A(.{0,}baz)\Z")
+        """Tests matching with multiple wildcards in a row."""
+        pattern = glob_to_regex("**baz")
+        self.assertRegex("agsgsbaz", pattern, "** should match any string")
+        self.assertRegex("baz", pattern, "** should match the empty string")
+        self.assertEqual(pattern.pattern, r"\A(.{0,}baz)\Z")
 
-        pat = glob_to_regex("*?baz")
-        self.assertTrue(pat.match("agsgsbaz"), "*? should match any string")
-        self.assertTrue(pat.match("abaz"), "*? should match a single char")
-        self.assertFalse(pat.match("baz"), "*? should not match the empty string")
-        self.assertEqual(pat.pattern, r"\A(.{1,}baz)\Z")
+        pattern = glob_to_regex("*?baz")
+        self.assertRegex("agsgsbaz", pattern, "*? should match any string")
+        self.assertRegex("abaz", pattern, "*? should match a single char")
+        self.assertNotRegex("baz", pattern, "*? should not match the empty string")
+        self.assertEqual(pattern.pattern, r"\A(.{1,}baz)\Z")
 
-        pat = glob_to_regex("a?*?*?baz")
-        self.assertTrue(pat.match("a g baz"), "?*?*? should match 3 chars")
-        self.assertFalse(pat.match("a..baz"), "?*?*? should not match 2 chars")
-        self.assertTrue(pat.match("a.gg.baz"), "?*?*? should match 4 chars")
-        self.assertEqual(pat.pattern, r"\A(a.{3,}baz)\Z")
+        pattern = glob_to_regex("a?*?*?baz")
+        self.assertRegex("a g baz", pattern, "?*?*? should match 3 chars")
+        self.assertNotRegex("a..baz", pattern, "?*?*? should not match 2 chars")
+        self.assertRegex("a.gg.baz", pattern, "?*?*? should match 4 chars")
+        self.assertEqual(pattern.pattern, r"\A(a.{3,}baz)\Z")
